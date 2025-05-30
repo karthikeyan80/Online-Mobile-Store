2a3f0c47072ca528927a26a80d4359c1c31721ff
karthikeyan80 <karthikeyan40581@gmail.com>
2025-05-30 21:33:23 +0530

    implemented authentication for admin panel

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { assets } from "../assets/assets";
-const Navbar = () => {
+const Navbar = ({setToken}) => {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between ">
       <img src={assets.logo} alt="logo" style={{ width: '150px', height: 'auto' }} />
-      <button className="bg-gray-600 text-white px-5 sm:px-7 sm:py-2  rounded-full text-xs sm:text-sm ">LOG OUT</button>
+      <button onClick={()=>setToken('')} className="bg-gray-600 text-white px-5 sm:px-7 sm:py-2  rounded-full text-xs sm:text-sm ">LOG OUT</button>
     </div>
   );
 };
